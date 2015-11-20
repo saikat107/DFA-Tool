536beab4cdc58d3dc875a3f5b6bd065559515d90
saikat107 <saikatch107@gmail.com>
2015-11-20 11:35:54 +0600

    Adda Help comment for running

--- Java/DFAtool-Java/src/com/google/sites/saikatch107/dfatool/Main.java
+++ Java/DFAtool-Java/src/com/google/sites/saikatch107/dfatool/Main.java
@@ -9,8 +9,10 @@ import com.google.sites.saikatch107.dfatool.dfa.Dfa;
 public class Main {
 
     /**
-     * @param args the command line arguments
-     */
+     * Create a Dfa with the DFA definition filename(with path) as argument
+     * if the DFA contruction is successful You will see a message "DFA contruction successful"
+     * Simulate a string with DFA by calling dfa.simulateDfa(string);
+     **/
     public static void main(String[] args) {
         // TODO code application logic here
         Dfa dfa = new Dfa("settings.txt");
