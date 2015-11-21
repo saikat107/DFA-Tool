11e5479a3ebc41c4ad1e00d770f81c63ccdc319e
saikat <saikatch107@gmail.com>
2015-11-21 19:26:49 +0600

    New Example of DFA added

--- Java/DFAtool-Java/src/com/google/sites/saikatch107/dfatool/Main.java
+++ Java/DFAtool-Java/src/com/google/sites/saikatch107/dfatool/Main.java
@@ -10,14 +10,14 @@ public class Main {
 
     /**
      * Create a Dfa with the DFA definition filename(with path) as argument
-     * if the DFA contruction is successful You will see a message "DFA contruction successful"
+     * if the DFA construction is successful You will see a message "DFA construction successful"
      * Simulate a string with DFA by calling dfa.simulateDfa(string);
      **/
     public static void main(String[] args) {
         // TODO code application logic here
-        Dfa dfa = new Dfa("settings.txt");
-        //dfa.debug();
-        dfa.simulateDfa("0101");
+        Dfa dfa = new Dfa("definition.txt");
+        //dfa.debug(); 
+        dfa.simulateDfa("abbaabbbab");
     }
     
 }
